d3e2e70735055f18015da9f8c5a26cba100f5505
cem-sirin <77017488+cem-sirin@users.noreply.github.com>
2024-07-11 23:51:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+short_description = (
+    "A package for computing the Multiplex Adamic-Adar score, a measure of node similarity in multiplex networks."
+)
+
 setup(
     name="mplexaa",
-    version="0.1",
+    version="0.1.2",
     packages=find_packages(),
+    author="Cem Sirin",
+    description=short_description,
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/cem-sirin/mplexaa",
